8b681618af582fa78e133647abbd60ffd70e1b40
Slobodan Blazeski <slobodan.blazeski@gmail.com>
2022-02-08 11:39:42 +0100

    Multi Level fitting

--- src/mesh/coarse.py
+++ src/mesh/coarse.py
@@ -3,6 +3,7 @@ from collections import OrderedDict
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from src.shared.gaussian import Gaussian
 from src.shared.faces import make_cube_faces
@@ -14,35 +15,19 @@ from src.shared.sides import (
 )
 
 class Coarse(nn.Module):
-    def __init__(self, n, kernel=7, sigma=1, r=0.5, ch=32, bias=True):
-        super(Coarse, self).__init__()        
+    def __init__(self, ns, kernel=3, sigma=1, r=0.5, ch=32, bias=True):
+        super(Coarse, self).__init__()
+        n = ns[-1]
         self.n = n
         phi, theta = make_phi_theta(n)
         self.register_buffer('phi', phi)
         self.register_buffer('theta', theta)
         self.register_buffer('faces', make_cube_faces(n))
         
-        nx =  n + (2*3)
-        sphere1  = to_spherical(sphered_vertices(nx, r))
-        bc = torch.zeros(6, 2, nx, nx) + r
-        sphere1 = torch.cat((sphere1, bc), dim=1)
-        self.register_buffer('sphere1', sphere1)
-        self.register_buffer('sphere2', sphered_vertices(n, r))
-        
-        self.net = nn.Sequential(OrderedDict([
-            ('conv1', nn.Conv2d(5, ch, 3, bias=bias)),
-            ('relu1', torch.nn.ReLU()), # nn.LeakyReLU(0.1)
-            ('conv2', nn.Conv2d(ch, ch, 3, bias=bias)),
-            ('relu2', torch.nn.ReLU()), # nn.LeakyReLU(0.1)
-            ('conv3', nn.Conv2d(ch, 3, 3, bias=bias)),
-            ('act3', torch.nn.Sigmoid()),
-        ]))
         self.abc = nn.Parameter(torch.zeros(1, 3, 1, 1))
-        self.radii = torch.nn.Parameter(torch.zeros(6, 3, n, n))
-        #self.abc = nn.Parameter(torch.randn(3))
-        #self.radii = torch.nn.Parameter(torch.randn(6, 3, n, n))
-        #self.abc = nn.Parameter(torch.rand(3)-0.5)
-        #self.radii = torch.nn.Parameter(torch.rand(6, 3, n, n))
+        self.radii = nn.ParameterList([
+            nn.Parameter(torch.zeros(6, 3, l, l)) for l in ns])        
+        
 
         self.gaussian =  Gaussian(kernel, sigma=sigma)
    
@@ -58,9 +43,15 @@ class Coarse(nn.Module):
         z = radii[:, 2] * torch.cos(self.theta)
         return torch.stack((x, y, z), dim=1) 
     
-    def forward(self):        
-        #radii = self.net(self.sphere1) + 0.5
-        radii = torch.sigmoid(self.gaussian(self.radii) +self.abc)
+    def scale(_, t, sz):
+        return F.interpolate(t, sz, mode='bilinear', align_corners=True)
+    
+    def forward(self): 
+        radii = self.gaussian(self.radii[0])
+        for high in self.radii[1:]:            
+            radii = self.scale(radii, high.size(-1)) + self.gaussian(high)
+            
+        radii = torch.sigmoid(radii +self.abc)
         #abc = torch.sigmoid(radii + self.abc) 
         stacked = self.get_ellipsoidal(radii)
         vert = to_vertices(stacked)
@@ -71,4 +62,3 @@ class Coarse(nn.Module):
 
     def load(self, filename):
         self.radii = nn.Parameter(torch.load(filename))
-    
